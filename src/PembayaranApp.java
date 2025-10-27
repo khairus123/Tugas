8666bbaa3c0ba5dc25b8234180512fd482f21436
ASUS OLED <khairusshalih58@gmail.com>
2025-10-27 21:57:56 +0700

    baru2

--- src/PembayaranApp.java
+++ src/PembayaranApp.java
@@ -1,7 +1,11 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class PembayaranApp {
     public static final double DISKON = 0.1;
     public static final int HARGA = 8500000;
+    public static final double PPN = 0.10;
+
 
     public static void main(String[] args) {
         Pembeli pembeli = new Pembeli();
@@ -18,10 +22,19 @@ public class PembayaranApp {
         System.out.println("Barang       : " + pembeli.getBarang());
         System.out.println("Harga        : " + pembeli.getHarga());
         System.out.println("Diskon       : " + hitungDiskon(pembeli) + "%");
-        System.out.println("Total Bayar  : " + total);
 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String waktuTransaksi = LocalDateTime.now().format(formatter);
+        System.out.println("Waktu Transaksi: " + waktuTransaksi);
+
+        System.out.println("Total Bayar  : " + total);
         pembeli.cetakNota();
     }
+
+    private static double hitungPajak(double totalSebelumPajak) {
+        return totalSebelumPajak * PPN;
+    }
+
 
     private static double hitungTotal(Pembeli pembeli) {
         return pembeli.getHarga() - (pembeli.getHarga() * pembeli.getDiskon());
